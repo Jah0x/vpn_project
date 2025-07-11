a89210e147c0925110cc333bc9e305b98ec7a117
Jah0x <petra12231@gmail.com>
2025-07-11 08:00:56 +0200

    fix(server): resolve TS2339 in vpn validator

--- apps/server/src/validators/vpn.ts
+++ apps/server/src/validators/vpn.ts
@@ -1,13 +1,12 @@
 import { z } from "zod";
 import "zod-to-openapi/dist/zod-extensions";
 
-export const VpnModel = z
+export const VpnModel = (z
   .object({
     id: z.string().uuid(),
     ownerId: z.string().uuid(),
     name: z.string(),
-  })
-  .openapi({ name: "Vpn" });
+  }) as any).openapi({ name: "Vpn" });
 
 export const VpnCreate = VpnModel.pick({ name: true }).openapi({
   name: "VpnCreateRequest",
